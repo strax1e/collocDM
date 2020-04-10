6ed93c129ba7721e151dfa57d035411c34e910a2
strax1e <strax1e@yandex.ru>
2020-04-10 23:45:07 +0300

    d

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "funcs.h"
+
+using namespace std;
+
+int main()
+{
+
+    vector<int> i(5, 5);
+    i[0] = 1;
+i.resize(6);
+    for (auto g = i.begin(); g != i.end(); g++)
+        cout << *g << ' ';
+
+
+    return 0;
+}
