6f88339176499241bb4a6cc6dd073728a3c0f872
strax1e <strax1e@yandex.ru>
2020-04-11 19:12:44 +0300

    testcommit

--- main.cpp
+++ main.cpp
@@ -7,10 +7,8 @@ int main()
 
     vector<int> i(5, 5);
     i[0] = 1;
-i.resize(6);
+    i.resize(6);
     for (auto g = i.begin(); g != i.end(); g++)
         cout << *g << ' ';
-
-
     return 0;
 }
